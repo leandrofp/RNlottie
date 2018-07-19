3e54d052ab2415c01bace9837390c8b7712f665e
LeandroFP <leandro.fernandezp@gmail.com>
2018-07-19 15:19:20 -0300

    First Commit

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+console.disableYellowBox = true;
+
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Animation from 'lottie-react-native';
+
+import anim from './search.json';
+
+export default class App extends Component {
+  componentDidMount() {
+    this.animation.play();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to Lottie Animations :-)</Text>
+        <View>
+          <Animation
+            ref={animation => {
+              this.animation = animation;
+            }}
+            style={{
+              width: 80,
+              height: 80
+            }}
+            loop={true}
+            source={anim}
+          />
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#A6207E'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff'
+  }
+});
+
+AppRegistry.registerComponent('App', () => App);
