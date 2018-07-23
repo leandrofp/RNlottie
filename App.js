76bda0bddc6c2b15c468d551f13b01efa853b025
LeandroFP <leandro.fernandezp@gmail.com>
2018-07-23 17:48:08 -0300

    Cambio de color en el JSON, falta renderizarlo de nuevo

--- App.js
+++ App.js
@@ -4,18 +4,64 @@
  * @flow
  */
 
+//LINEA 546 JSON,  PRIMER VECTOR DE CAPA 1 
+/*"k":[
+                           0.204,
+                           0.38,
+                           1,
+                           1
+                        ],
+*/
+
+
 console.disableYellowBox = true;
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View , TouchableHighlight} from 'react-native';
 import Animation from 'lottie-react-native';
 
-import anim from './search.json';
+import anim from './data1.json';
 
 export default class App extends Component {
-  componentDidMount() {
-    //this.animation.play();
+
+  constructor(props){
+    super(props)
+
+    this.state={source : anim}
+
+    this.green = this.green.bind(this)
+    this.greenPlay = this.greenPlay.bind(this)
+
   }
+
+
+  componentDidMount() {
+    this.animation.play();
+  }
+
+  green(){
+    //this.animation.stop();
+    //this.state.source.layers[1].shapes[0].it[1].c.k[0] = '0'
+    //this.state.source.layers[1].shapes[0].it[1].c.k[1] = '1'
+    //this.state.source.layers[1].shapes[0].it[1].c.k[2] = '0'
+    //this.state.source.layers[1].shapes[0].it[1].c.k[3] = '0'
+
+     anim.layers[1].shapes[0].it[1].c.k[0] = '0'
+     anim.layers[1].shapes[0].it[1].c.k[1] = '1'
+     anim.layers[1].shapes[0].it[1].c.k[2] = '0'
+     anim.layers[1].shapes[0].it[1].c.k[3] = '0'
+ 
+    //console.log(this.state.source.layers[1])
+    this.animation.reset();
+    //console.log(this.animation.props.source.layers[1].shapes[0].it[1].c.k)
+    this.greenPlay();
+  }
+  greenPlay(){
+      console.log("pase por play")
+      //console.log(this.state.source.layers[1].shapes[0].it[1].c.k)
+      this.animation.play();
+  }
+
 
   render() {
     return (
@@ -27,14 +73,19 @@ export default class App extends Component {
               this.animation = animation;
             }}
             style={{
-              width: 80,
-              height: 80
+              width: 100,
+              height: 100,
+              //backgroundColor: 'blue' //el fondo te deja editarlo desde aca
             }}
-            loop={true}
+            loop={false}
+            //source={this.state.source}
             source={anim}
           /> 
 
-
+        <TouchableHighlight style={styles.greenButton} onPress={this.green}>
+                <Text style={styles.buttonText}>Hacete Verde</Text>
+        </TouchableHighlight>
+       
         </View>
 
       </View>
@@ -54,7 +105,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
     color: '#ffffff'
-  }
+  },
+  greenButton: {
+    backgroundColor: '#FFFFFF',
+    paddingVertical: 15,
+    width: 90,
+    //marginRight: 4,
+    borderRadius: 5,
+    elevation: 2, // Android
+    //alignItems:'flex-end'
+  },
 });
 
 AppRegistry.registerComponent('App', () => App);
